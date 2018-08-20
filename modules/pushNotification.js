6e8ca9ce7173535d12875973c49e1a4d2abc2c1b
danchab <banchabala@gmail.com>
2018-08-20 22:39:36 +0200

    get item details by affiliate id

--- modules/pushNotification.js
+++ modules/pushNotification.js
@@ -12,23 +12,30 @@ request({
 }, (error, response , body) => {
     if (!error && response.statusCode === 200) {
         console.log(`body ${JSON.stringify(body, undefined,2)}`)
+        if ( body === null) {
+            console.log('Not affialiate wiht notification')
+        }else {
+            body.forEach((element) => {
+                let affiliateId = element._id
+
+    request({
+        url: 'https://vicoupon-api.herokuapp.com/notification/sendProductDetails',
+        body: {
+            affiliateId: affiliateId
+        },
+        method: 'POST',
+        json: true
+    }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            console.log(`body: ${body}`);
+            
+        }else {
+            console.error('Could not get product details and userId')
+        }
+    })
+            })
+        }
     }else {
         console.log('Error: Could not get affiliates with notification')
     }
 })
-
-// request({
-//     url: 'https://vicoupon-api.herokuapp.com//notification/sendProductDetails',
-//     body: {
-//         affiliateId: affiliateId
-//     },
-//     method: 'POST',
-//     json: true
-// }, (error, response, body) => {
-//     if (!error && response.statusCode === 200) {
-//         console.log(`body: ${body}`);
-        
-//     }else {
-//         console.error('Could not get product details and userId')
-//     }
-// })
