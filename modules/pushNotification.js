545933180f91534885ba73a5803716649327aca3
danchab <banchabala@gmail.com>
2018-08-21 01:24:36 +0200

    push notification

--- modules/pushNotification.js
+++ modules/pushNotification.js
@@ -40,7 +40,64 @@ request({
     }
 
     const createMgTemplate = (body) => {
+        let elements = []
+
         let productArray = body.products
-        console.log(`productArray: ${productArray}`)
+        productArray.forEach((product) => {
+            let productPrice = product.productPrice
+            let productImage = product.productImage
+            let affiliateId = product.affiliateId
+
+            elements.push({
+                "title":`${productPrice}`,
+                "image_url":productImage,
+                "subtitle":`${productPrice}`,
+                "buttons":[
+                  {
+                    "type": "postback",
+                    "title": "🔖 Get Coupon ",
+                    "payload": `GET_COUPON#${affiliateId}`
+                  }            
+                ]      
+              })
+        })
+
+        let msgContent = {
+            "message":{
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"generic",
+                "sharable": true,
+                elements
+              }
+            }
+          }
+        }
+
+    let usersDetails = body.usersDetails
+    usersDetails.forEach((userDetails) => {
+        let fbId = userDetails.fbId
+        let fbName = userDetails.fbName
+
+        let messageData = {
+            recipient: {
+              id: fbId
+            },
+            message: msgContent
+          }
+          request({
+            url: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: {access_token: PAGE_ACCESS_TOKEN},
+            method: 'POST',
+            json: messageData
+        
+          }, (error, response, body) => {
+            if (!error && response.statusCode == 200){
+              console(`Notification sent to fbId: ${messageData.recipient.id}`)
+             }
+            
+        })
+    })
     }
 })
