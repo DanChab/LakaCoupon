390d7ddfcdde3359a4787a65e995298cc45358f9
danchab <banchabala@gmail.com>
2018-08-20 11:59:06 +0200

    getting all affiliates with notification

--- modules/pushNotification.js
+++ modules/pushNotification.js
@@ -3,17 +3,32 @@ const request = require('request')
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
 
 request({
-    url: 'https://vicoupon-api.herokuapp.com/sendProductDetails',
+    url: 'https://vicoupon-api.herokuapp.com//notification/affiliateWithNotification',
     body: {
-        affiliateId: affiliateId
+        hasNotification: '1'
     },
     method: 'POST',
     json: true
-}, (error, response, body) => {
+}, (error, response , body) => {
     if (!error && response.statusCode === 200) {
-        console.log(`body: ${body}`);
-        
+        console.log(`body ${JSON.stringify(body, undefined,2)}`)
     }else {
-        console.error('Could not get product details and userId')
+        console.log('Error: Could not get affiliates with notification')
     }
 })
+
+// request({
+//     url: 'https://vicoupon-api.herokuapp.com//notification/sendProductDetails',
+//     body: {
+//         affiliateId: affiliateId
+//     },
+//     method: 'POST',
+//     json: true
+// }, (error, response, body) => {
+//     if (!error && response.statusCode === 200) {
+//         console.log(`body: ${body}`);
+        
+//     }else {
+//         console.error('Could not get product details and userId')
+//     }
+// })
